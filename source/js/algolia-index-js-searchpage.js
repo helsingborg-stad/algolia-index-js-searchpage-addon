f06a376fd7455f4ad416109e6bb2d43acd06475a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-25 14:34:42 +0200

    Search input

--- source/js/algolia-index-js-searchpage.js
+++ source/js/algolia-index-js-searchpage.js
@@ -32,16 +32,20 @@ class AlgoliaIndexJsSearchpage {
         searchResults && searchResults.nbHits !== 0 ? (
           children
         ) : (
-          <div className="notice info">
-            <i className="fa fa-info-circle"></i>
-            {algoliaTranslations.noresult} "{searchState.query ? searchState.query : qs.parse(location.search.slice(1)).s}".            
+          <div className="c-notice c-notice--info">
+            <span class="c-notice__icon">
+              <i className="c-icon c-icon--size-md material-icons">report</i>
+            </span> 
+            <span class="c-notice__message--sm">
+              {algoliaTranslations.noresult} "{searchState.query ? searchState.query : qs.parse(location.search.slice(1)).s}".    
+            </span>
           </div>
         )
     );
 
     //Results list
     const Hits = ({ hits }) => (
-      <ul className="c-searchresult">
+      <section className="c-searchresult">
         {hits.map(hit => (
           <div key={hit.objectID} className="c-searchresult__item">
 
@@ -76,24 +80,25 @@ class AlgoliaIndexJsSearchpage {
 
           </div>
         ))}
-      </ul>
+      </section>
     );
 
     //Search input
     const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
-      <form className="c-searchform" noValidate="noValidate" action="/" role="search" itemProp="potentialAction" itemScope="" itemType="http://schema.org/SearchAction">
-        <label htmlFor="searchkeyword" className="sr-only c-searchform__label">{algoliaTranslations.submit}</label>
-        <input
-          id="searchkeyword"
-          className="c-searchform__input"
-          autoComplete="off"
-          type="search"
-          value={currentRefinement}
-          onChange={event => refine(event.currentTarget.value)}
-          required=""
-          placeholder={algoliaTranslations.placeholder}
-          name="s"
-        />
+      <form id="" class="c-form" noValidate="noValidate" action="/" role="search" itemProp="potentialAction" itemScope="" itemType="http://schema.org/SearchAction">
+        <div id="" class="c-field c-field__text">
+          <input
+            id="searchkeyword"
+            className="c-searchform__input"
+            autoComplete="off"
+            type="search"
+            value={currentRefinement}
+            onChange={event => refine(event.currentTarget.value)}
+            required=""
+            placeholder={algoliaTranslations.placeholder}
+            name="s"
+          />
+        </div>
       </form>
     );
 
