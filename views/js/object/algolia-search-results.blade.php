22c1a7742cbba8fa1502a7fcc7c2cf2e7c163357
niclas.norin <niclas.norin@klingit.team>
2022-06-20 13:27:54 +0200

    test: Added dummy content

--- views/js/object/algolia-search-results.blade.php
+++ views/js/object/algolia-search-results.blade.php
@@ -1,7 +1,15 @@
 <div>
-    @card 
+    @card([
+      "heading" => "Tekniksommarskola – programmering",
+      "subheading" => "Helsingborg.se",
+      "content" => "Under tre halvdagar kommer vi att lära oss att programmera och hur robotar kan styras på olika sätt. Vi kommer även att fundera vidare på hur vi kan utveckla våra förmågor inom matematiken bakom programmering. Bland annat kommer vi att testa dansprogrammering, Kojo, Edison, Arduino och Micro:bit. Vi bygger egna enklare robotar och pratar om sekvens, styrning och om skillnaden mellan block- och textprogrammering. Du åker gratis i abonnerad buss. Dryck och enkel lunch serveras. Dag: 28-30 juni Tid: 10.00-13.00 Ålder: 12-16 år Anmälan: Skicka e-post till annacarin.jendland@landskrona.se och uppge ditt namn plus en förälders namn samt mejladress och telefonnummer till både dig och en förälder. Uppge även klass och skola du går i. Begränsat antal platser. Arrangör: Landskrona stad tillsammans med Lunds universitet och Vattenhallen Science Center"
+    ])
 
-    <div data-inject-here>Content</div>
+    <div data-inject-heading>
+      <h2>Tekniksommarskola – programmering</h2>
+      <h4>Helsingborg.se</h4>
+      
+    </div>
     @endcard
 
 
