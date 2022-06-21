945b9f4f35c04c300c624f36301689db9073c5d0
cedrikvonheiroth <cedrikvonheiroth@gmail.com>
2022-06-21 13:36:39 +0200

    input padding and formatting

--- views/js/object/algolia-search-results.blade.php
+++ views/js/object/algolia-search-results.blade.php
@@ -1,10 +1,8 @@
- @card([
-      "heading" => "_HIT_HEADING",
-      "subHeading" => "_HIT_SUBHEADING",
-      "content" => "_HIT_EXCERPT",
-      "image" => "_HIT_IMAGE",
-      "link" => "_HIT_LINK",
-    ])
-    
-    @endcard
-     
+@card([
+  "heading" => "_HIT_HEADING",
+  "subHeading" => "_HIT_SUBHEADING",
+  "content" => "_HIT_EXCERPT",
+  "image" => "_HIT_IMAGE",
+  "link" => "_HIT_LINK",
+])
+@endcard
