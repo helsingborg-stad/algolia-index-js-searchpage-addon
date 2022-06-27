9d74fb9d2d28ce42fd5a08ea49378bc550b22a78
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-06-27 14:17:46 +0200

    Use blade file to create markup for searchpage

---
+++ views/search-page.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+  <div class="search-panel">
+    <div class="search-panel__filters">
+      <div id="dynamic-widgets">
+      </div>
+    </div> 
+    <div class="search-panel__results">
+      <div id="searchbox"></div>
+      <div class="o-grid">
+          <div id="hits"></div>
+      </div>
+      <div id="pagination"></div>
+    </div>
+  </div>
+</div>
