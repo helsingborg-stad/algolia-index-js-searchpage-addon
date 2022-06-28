7193e26a3c4bbe826bdae5180501c20f75dd4d12
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-06-28 16:25:01 +0200

    Remove unused file

--- views/js/object/algolia-search-results-noimg.blade.php
+++
@@ -1,8 +0,0 @@
-@card([
-  "heading" => "_HIT_HEADING",
-  "subHeading" => "_HIT_SUBHEADING",
-  "content" => "_HIT_EXCERPT",
-  "link" => "_HIT_LINK",
-  "classList" => ['c-card--size-md'],
-])
-@endcard
