3da963edbd275e6e2af1c5b622ba81026681107e
niclas.norin <niclas.norin@klingit.team>
2022-06-29 10:51:05 +0200

    Feat: Changes pagination to custom component

--- source/js/algolia-index-js-searchpage.js
+++ source/js/algolia-index-js-searchpage.js
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js';
-import { connectSearchBox } from 'instantsearch.js/es/connectors'; 
+import { connectSearchBox, connectPagination } from 'instantsearch.js/es/connectors'; 
 import { searchBox, hits, configure, pagination } from 'instantsearch.js/es/widgets';
 
 
@@ -16,6 +16,7 @@ const searchClient = algoliasearch(
 const search = instantsearch({
   indexName: algoliaSearchData.indexName,
   searchClient,
+
 });
 
 function spinner (state) {
@@ -81,10 +82,106 @@ const customSearchBox = connectSearchBox(
 search.addWidgets([
   customSearchBox({
     container: document.querySelector('#searchbox'),
+    queryHook(query, search) { search("sommar"); },
+
+
   })
 ]);
 
 /* End searchbox */ 
+
+/* Pagination */
+// Create the render function
+const renderPagination = (renderOptions, isFirstRender) => {
+  const {
+    pages,
+    currentRefinement,
+    nbPages,
+    isFirstPage,
+    isLastPage,
+    refine,
+    createURL,
+  } = renderOptions;
+
+  const container = document.querySelector('#pagination');
+  let paginationHtml =algoliaSearchComponents["pagination-item"].html;
+
+    container.innerHTML = `
+    <ul class="c-pagination">
+      ${!isFirstPage
+      ? `
+            <li>
+              <a
+                href="${createURL(0)}"
+                data-value="${0}"
+              >
+                First
+              </a>
+            </li>
+            <li>
+              <a
+                href="${createURL(currentRefinement - 1)}"
+                data-value="${currentRefinement - 1}"
+              >
+                Previous
+              </a>
+            </li>
+            `
+      : ''
+    }
+    
+      ${pages
+      .map(
+        page => paginationHtml
+          .replace("{ALGOLIA_JS_PAGINATION_TEXT}", page + 1)
+          .replace("{ALGOLIA_JS_PAGINATION_HREF}", createURL(page))
+          .replace("{ALGOLIA_JS_PAGINATION_COLOR}", currentRefinement === page ? 'primary' : 'default')
+          .replace("{ALGOLIA_JS_PAGINATION_CLASS}", currentRefinement === page ? 'c-pagination--is-active' : '')
+          .replace("{ALGOLIA_JS_PAGINATION_PAGE_NUMBER}", page)
+      )
+      .join('')}
+        ${!isLastPage
+      ? 
+      paginationHtml
+        .replace("{ALGOLIA_JS_PAGINATION_TEXT}", "Next")
+        .replace("{ALGOLIA_JS_PAGINATION_HREF}", createURL(currentRefinement + 1))
+        .replace("{ALGOLIA_JS_PAGINATION_COLOR}", 'default')
+        .replace("{ALGOLIA_JS_PAGINATION_CLASS}", '')
+        .replace("{ALGOLIA_JS_PAGINATION_PAGE_NUMBER}", currentRefinement + 1) +
+      paginationHtml
+        .replace("{ALGOLIA_JS_PAGINATION_TEXT}", "Last")
+        .replace("{ALGOLIA_JS_PAGINATION_HREF}", createURL(nbPages - 1))
+        .replace("{ALGOLIA_JS_PAGINATION_COLOR}", 'default')
+        .replace("{ALGOLIA_JS_PAGINATION_CLASS}", '')
+        .replace("{ALGOLIA_JS_PAGINATION_PAGE_NUMBER}", nbPages - 1)
+
+      : ''
+    }
+    </ul>
+  ` ; 
+
+  [...container.querySelectorAll('a')].forEach(element => {
+    element.addEventListener('click', event => {
+      event.preventDefault();
+      refine(event.currentTarget.dataset.value);
+    });
+  });
+};
+
+// Create the custom widget
+const customPagination = connectPagination(
+  renderPagination
+);
+
+// Instantiate the custom widget
+search.addWidgets([
+  customPagination({
+    container: document.querySelector('#pagination'),
+  })
+]);
+
+/* end pagination */
+
 
 
 search.addWidgets([
@@ -155,6 +252,7 @@ search.addWidgets([
         post_excerpt: item._highlightResult['post_excerpt'].value.replace("[&amp;hellip;]", "...").replace('&amp;#038;', "&"),
         post_title: item._highlightResult['post_title'].value.replace('&amp;#038;', "&"),
       }));
+      
     },
 
     escapeHTML: false,
@@ -162,28 +260,9 @@ search.addWidgets([
   }),
   configure({
     hitsPerPage: 8,
+    
   }),
-  pagination({
-    container: '#pagination',
-    showFirst: false,
-    showLast:false,
-    cssClasses: {
-      root: 'pagination',
-      link: [
-      'c-button', 
-      'c-pagination__link',
-      'c-button--sm',
-      'c-button__transparent',
-    ],
-      selectedItem: [
-        'c-c-pagination__link',
-        'c-pagination__item--is-active',
-        'c-button__filled',
-        'c-button__filled--primary',
-        'c-button--sm'
-      ],
-    },
-  }),
+
 ]);
 
 search.start();
