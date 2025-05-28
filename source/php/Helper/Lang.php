fcfa90538c28e919e8b26c9dccfcbbfd6bea5bb0
Petter Andersson <petter.andersson@helsingborg.se>
2025-05-28 07:51:52 +0000

    update translations

--- source/php/Helper/Lang.php
+++ source/php/Helper/Lang.php
@@ -8,9 +8,8 @@ class Lang
     {
         return [
             'searchLabel' => __('What are you looking for?', 'algolia-index-js-searchpage'),
-            'noresults' => sprintf(__('No search results found on your query %s', 'algolia-index-js-searchpage'), '"{ALGOLIA_JS_SEARCH_QUERY}"'),
-            'algoliaStats' => sprintf(__('%s search results found in %s', 'algolia-index-js-searchpage'), 
-            '{ALGOLIA_JS_STATS_COUNT}', '{ALGOLIA_JS_STATS_QUERY}')
+            'noresults' => __('No search results found on your query.', 'algolia-index-js-searchpage'),
+            'algoliaStats' => sprintf(__('%s search results found.', 'algolia-index-js-searchpage'), '{ALGOLIA_JS_STATS_COUNT}')
         ];
     }
 }
