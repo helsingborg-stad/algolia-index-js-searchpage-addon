287db1e7d2c2419df5abfe1427c22997bece181f
Petter Andersson <petter.andersson@helsingborg.se>
2025-05-30 06:53:20 +0000

    fix padding

--- views/partials/hits.blade.php
+++ views/partials/hits.blade.php
@@ -1,6 +1,12 @@
 @element([
     'attributeList' => [
         'data-js-search-page-hits' => true
+    ],
+    'classList' => [
+        'u-display--flex',
+        'u-flex--gridgap',
+        'u-flex-direction--column',
+        'unlist'
     ]
 ])
     @include('partials.loader')
