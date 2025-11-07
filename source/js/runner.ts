1eed0f8596fa2b7cc124f301155c446c645dcf94
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-11-07 15:03:56 +0100

    fix: move re-rendering lock.

--- source/js/runner.ts
+++ source/js/runner.ts
@@ -13,40 +13,48 @@ export const Runner = (
   html: HtmlRenderService
 ): HtmlRunnerService => {
   // Perfom search with the provided query
+  let isRenderingFacets = false;
+
+  // Register facet events ONCE
+  const facetsContainer = html.getFacetsContainer();
+  if (facetsContainer) {
+    binder.registerFacets(facetsContainer, (facetFilters: string[][]) => {
+      if (!isRenderingFacets) {
+        exec({
+          ...lastParams,
+          page: 1,
+          facetFilters,
+        });
+      }
+    });
+  }
+
+  let lastParams: GenericSearchQueryParams = {};
   const exec = (params: GenericSearchQueryParams) => {
+    lastParams = params;
     adapter.search(params).then(result => {
-      setUrlSearchParam(params.query)
-  html.reset()
-  html.renderStats(result)
-  html.renderFacets(result, params.facetFilters)
-  html.renderItems(result)
-  html.renderPagination(result)
+      setUrlSearchParam(params.query);
+      html.reset();
+      html.renderStats(result);
+      isRenderingFacets = true;
+      html.renderFacets(result, params.facetFilters);
+      isRenderingFacets = false;
+      html.renderItems(result);
+      html.renderPagination(result);
       binder.registerPagination(
         html.getPaginationContainer(),
         (page: number) => {
           exec({
             ...params,
             page,
-          })
+          });
         }
-      )
-      // Register facet events if facets container exists
-      const facetsContainer = html.getFacetsContainer()
-      if (facetsContainer) {
-        binder.registerFacets(facetsContainer, (facetFilters: string[][]) => {
-          exec({
-            ...params,
-            page: 1, // Reset to first page when filtering
-            facetFilters,
-          })
-        })
-      }
-    })
-  }
-  // Register event handlers and callback for input-field
-  binder.registerSearchBox(html.getInputField(), exec)
+      );
+    });
+  };
+  binder.registerSearchBox(html.getInputField(), exec);
 
   return {
     exec,
-  }
+  };
 }
