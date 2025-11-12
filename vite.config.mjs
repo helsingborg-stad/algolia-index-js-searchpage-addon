c1696fe8546f71408bd2eb2dcc8067babde04fb1
Petter Andersson <petter.andersson@helsingborg.se>
2025-11-12 16:04:32 +0100

    update parameters for mago

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,8 @@
 import { createViteConfig } from "vite-config-factory";
 
 const entries = {
-        'js/main': './source/js/main.ts',
-    'css/instantsearch': './source/sass/algolia-index-js-searchpage.scss',
+	"js/main": "./source/js/main.ts",
+	"css/instantsearch": "./source/sass/algolia-index-js-searchpage.scss",
 };
 
 export default createViteConfig(entries, {
