bf6a26ee677c245a849eb14417d837d769c51e77
GitHub Action <>
2025-11-13 18:38:29 +0000

    build: update package file versions to 3.3.16

--- algolia-index-js-searchpage.php
+++ algolia-index-js-searchpage.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Algolia Index Js Search Page
  * Plugin URI:        (#plugin_url#)
  * Description:       Replaces search page with a js (instant search) page.
- * Version: 3.3.15
+ * Version: 3.3.16
  * Author:            Sebastian Thulin
  * Author URI:        (#plugin_author_url#)
  * License:           MIT
