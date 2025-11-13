4de59c88e1ca5202bb755f89f0a5fe420bab737c
Petter Andersson <petter.andersson@helsingborg.se>
2025-11-13 14:20:11 +0100

    fix

--- source/js/mappers.ts
+++ source/js/mappers.ts
@@ -1,5 +1,5 @@
 export const decodeHtml = (html: string): string => {
-  const txt = document.createElement('textarea')
-  txt.innerHTML = html
-  return txt.value
-}
+	const txt = document.createElement("textarea");
+	txt.innerHTML = html;
+	return txt.value;
+};
